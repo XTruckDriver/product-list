91b1b67590236ca808e480d5fb0938832aa9bc26
Mark Smyth <xtruckdriver1@gmail.com>
2026-04-29 11:37:22 -0400

    add validation - ReviewSchema - require text and product

--- models/review.js
+++ models/review.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
   userName: String,
-  text: String,
-  product: {type: Schema.Types.ObjectId, ref: "product"},
+  text: { type: String, required: true },
+  product: {type: Schema.Types.ObjectId, ref: "product", required: true },
 });
 
 module.exports = mongoose.model("Review", ReviewSchema);
